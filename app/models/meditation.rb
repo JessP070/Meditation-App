e665130b38fa5f716d555edbf6f9525b538b2f5b
JessP070 <jcpereira070@gmail.com>
2020-09-04 14:44:26 -0700

    fixed has many

--- app/models/meditation.rb
+++ app/models/meditation.rb
@@ -2,6 +2,6 @@ class Meditation < ApplicationRecord
   belongs_to :technique
   belongs_to :user
   has_many :reviews
-  has_many :users through :reviews
+  has_many :users, through: :reviews
 
 end
