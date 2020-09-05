c5bd08e3e6c8f7dbccdacb2d1149007c41315e2b
JessP070 <jcpereira070@gmail.com>
2020-09-05 13:53:15 -0700

    add validations/has_secure_password

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
     has_many :reviewed_meditations, through: :reviews, source: :meditation
 
     has_many :meditations
+
+    validates :username, uniqueness: true, presence: true
+
+    has_secure_password
 end
