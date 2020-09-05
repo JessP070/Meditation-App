309ce73f808ca426a3ec5240afdc0173d858a08d
JessP070 <jcpereira070@gmail.com>
2020-09-05 13:52:35 -0700

    find user by user id

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,17 +7,19 @@ class UsersController < ApplicationController
         @user = User.new(user_params)
         if @user.save
             session[:user_id] = @user.id
-            redirect_to_user_path(@user)
+            redirect_to user_path(@user)
         else 
             render :new
         end
     end
 
     def show
-
+        @user = User.find_by_id(params[:id])
+        redirect_to '/' if !@user
     end
 
     private
+
     def user_params
         params.require(:user).permit(:username, :password)
     end
