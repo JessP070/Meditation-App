10b0157a3eb71b06e99482e7c4c65f60155b5fe7
JessP070 <jcpereira070@gmail.com>
2020-09-07 12:20:09 -0700

    change params/add redirect if failed auth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,11 +15,12 @@ class SessionsController < ApplicationController
 
     def create
         @user = User.find_by(username: params[:user][:username])
-        if @user && @user.authenticate(username: params[:user][:username])
+        if @user && @user.authenticate(params[:user][:password])
             session[:user_id] = @user.id
             redirect_to user_path(@user) 
         else
-            
+            flash[:error] = "Sorry, please try again. "
+            redirect_to login_path
 
         end
     end
