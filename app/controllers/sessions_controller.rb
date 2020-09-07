a5cdfca98a03fa127b6c187383e6603599442c1e
JessP070 <jcpereira070@gmail.com>
2020-09-07 12:07:04 -0700

    create a session if authenticated

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,4 +12,15 @@ class SessionsController < ApplicationController
     def new
 
     end
+
+    def create
+        @user = User.find_by(username: params[:user][:username])
+        if @user && @user.authenticate(username: params[:user][:username])
+            session[:user_id] = @user.id
+            redirect_to user_path(@user) 
+        else
+            
+
+        end
+    end
 end
