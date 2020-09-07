3c0aa87e430485f9e328e8b6d8041d85d616ea8d
JessP070 <jcpereira070@gmail.com>
2020-09-07 09:53:25 -0700

    add logout route

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   get '/signup' => 'users#new'
   post '/signup' => 'users#create'
 
+  delete '/logout' => 'sessions#destroy'
+
   resources :reviews
   resources :meditations
   resources :techniques
