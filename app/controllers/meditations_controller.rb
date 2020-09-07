6817d264de78bd1847170c0d86be63c2dc7378d4
JessP070 <jcpereira070@gmail.com>
2020-09-07 16:50:14 -0700

    define params

--- app/controllers/meditations_controller.rb
+++ app/controllers/meditations_controller.rb
@@ -1,6 +1,12 @@
 class MeditationsController < ApplicationController
     def new
         @meditation = Meditation.new
+        @meditation.build_technique
+    end
 
+    private
+
+    def meditation_params
+        params.require(:meditation.permit(:mood, :duration, :technique_id, technique_attributes[:name, :description])
     end
 end
