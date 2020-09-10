8d41c40b5f274b1f9df3b4d097905f0cc6bfde6e
JessP070 <jcpereira070@gmail.com>
2020-09-10 10:19:53 -0700

    create a new meditation

--- app/controllers/meditations_controller.rb
+++ app/controllers/meditations_controller.rb
@@ -4,9 +4,20 @@ class MeditationsController < ApplicationController
         @meditation.build_technique
     end
 
+    def create 
+        @meditation = Meditation.new(meditation_params)
+        @meditation.user_id = session[:user_id]
+        if @meditation.save
+            redirect_to meditation_path(@meditation)
+        else 
+            render :new
+        end
+    end
+
     private
 
     def meditation_params
-        params.require(:meditation.permit(:mood, :duration, :technique_id, technique_attributes[:name, :description])
+        params.require(:meditation).permit(:mood, :duration, :technique_id, technique_attributes: [:name, :description])
     end
+
 end
