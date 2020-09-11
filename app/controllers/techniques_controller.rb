5216bf200a4a37116cac03294002914187c21b73
JessP070 <jcpereira070@gmail.com>
2020-09-11 09:44:45 -0700

    add new method for technique

--- app/controllers/techniques_controller.rb
+++ app/controllers/techniques_controller.rb
@@ -2,5 +2,13 @@ class TechniquesController < ApplicationController
     def index
         @techniques = Technique.all
     end
+    
+    def new 
+        @technique = Technique.new
+    end
+
+
+  
+
 
 end
