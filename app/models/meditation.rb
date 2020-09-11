b2d614b9047b3605c4f54c1352b17da0146f5c71
JessP070 <jcpereira070@gmail.com>
2020-09-11 11:21:53 -0700

    remove method

--- app/models/meditation.rb
+++ app/models/meditation.rb
@@ -5,9 +5,6 @@ class Meditation < ApplicationRecord
   has_many :users, through: :reviews
   accepts_nested_attributes_for :technique
 
-  def technique_attributes=(attributes)
-    technique = Technique.find_or_create_by(attributes) if !name.empty?
-  end
 
 
 end
