4ab3b1354016c5cc75325970b833d024d5b0b56b
JessP070 <jcpereira070@gmail.com>
2020-09-11 09:45:19 -0700

    created new index, new, create, show method

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,36 @@
 class ReviewsController < ApplicationController
+
+    def index
+        if params[:meditation_id] && @meditation = Meditation.find_by_id(params[:meditation_id])
+            @reviews = @meditation.reviews
+        else 
+            @reviews = Review.all
+        end
+    end
+
+    def new 
+        @meditation = Meditation.find_by_id(params[:meditation_id])
+        @review = @meditation.reviews.build
+    end
+
+    def create 
+        @review = Review.new(review_params)
+        @review.user_id = session[:user_id]
+        if @review.save
+            redirect_to review_path(@review)
+        else 
+            render :new
+        end
+    end
+
+    def show
+        @review = Review.find_by_id(params[:id])
+    end
+
+
+    private
+    
+    def review_params
+        params.require(:review).permit(:stars, :title, :content, :meditation_id, meditation_attributes: [:mood, :duration])
+    end
 end
