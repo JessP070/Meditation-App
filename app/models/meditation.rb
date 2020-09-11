27b9e1f221a3c07bbfe21a2838915b6e0882bba1
JessP070 <jcpereira070@gmail.com>
2020-09-11 12:35:01 -0700

    add validations

--- app/models/meditation.rb
+++ app/models/meditation.rb
@@ -3,7 +3,15 @@ class Meditation < ApplicationRecord
   belongs_to :user
   has_many :reviews
   has_many :users, through: :reviews
+
   accepts_nested_attributes_for :technique
+
+  validates :mood, presence: true
+
+  def technique_attributes=(attributes)
+    technique = Tehnique.find_or_create_by(attributes) if
+    !attributes['name'].empty?
+  end
 
 
 
