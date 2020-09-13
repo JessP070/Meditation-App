2c28b4cd3448be47df58248382ef057963a32d7a
JessP070 <jcpereira070@gmail.com>
2020-09-12 19:36:55 -0700

    update path

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
 
   delete '/logout' => 'sessions#destroy'
 
-  get '/auth/google_oauth2/callback' => 'sessions#omniauth'
+  get '/auth/:provider/callback' => 'sessions#create'
 
   resources :reviews
   resources :meditations do 
