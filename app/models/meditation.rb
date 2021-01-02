979bc9363c8265bfd2775f4b3c8145f9ea54aece
JessP070 <jcpereira070@gmail.com>
2021-01-02 10:40:58 -0800

    add dependent

--- app/models/meditation.rb
+++ app/models/meditation.rb
@@ -2,7 +2,8 @@ class Meditation < ApplicationRecord
   belongs_to :technique
   belongs_to :user
   has_many :reviews
-  has_many :users, through: :reviews
+  has_many :users, through: :reviews, :dependent => :destroy
+
 
   accepts_nested_attributes_for :technique
 
@@ -17,20 +18,25 @@ class Meditation < ApplicationRecord
   end
 
   def technique_attributes=(attributes)
-    technique = Tehnique.find_or_create_by(attributes) if
-    !attributes['name'].empty?
+    self.technique = Technique.find_or_create_by(attributes) if !attributes['name'].empty?
+    self.technique
   end
 
+
   def not_a_duplicate
-    if Meditation.find_by(mood: mood, tecnique_id: technique_id)
+    if Meditation.find_by(mood: mood, technique_id: technique_id)
       errors.add(:mood, "Has already been added for that technique")
     end
   end
+
+  def technique_name
+  technique.try(:name)
+  end 
 
   def mood_and_technique
     "#{mood} - #{technique.name}"
   end
   
-
+ 
 end
 
