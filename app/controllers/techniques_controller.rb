fc21f4f4aa1c731c63c1267d1c3abe289805927b
JessP070 <jcpereira070@gmail.com>
2021-01-02 10:40:27 -0800

    deletion

--- app/controllers/techniques_controller.rb
+++ app/controllers/techniques_controller.rb
@@ -1,14 +1,7 @@
 class TechniquesController < ApplicationController
     def index
         @techniques = Technique.all
+
     end
     
-    def new 
-        @technique = Technique.new
-    end
-
-
-  
-
-
 end
