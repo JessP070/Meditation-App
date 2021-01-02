56cea3b2f8726ded8935269799ec142d83faea52
JessP070 <jcpereira070@gmail.com>
2021-01-02 10:39:12 -0800

    add private method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,20 @@
 class ApplicationController < ActionController::Base
+    helper_method :current_user, :logged_in?
 
-    def current_user 
-        @current_user ||= User.find_by_id(session[:user_id])
+    private
+  
+    def logged_in?
+      !!session[:user_id]
     end
-end
+  
+    def current_user
+      @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
+    end
+  
+    def redirect_if_not_logged_in
+      redirect_to login_path if !logged_in?
+    end
+  
+  
+  end
+
